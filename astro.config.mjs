f643b478ed8a5f6e5aa42b4223493b4f488a0432
Stanislas de Chevigné <standechevigne@gmail.com>
2026-04-14 16:14:20 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: cloudflare(),
   i18n: {
     defaultLocale: "fr",
     locales: ["fr", "en"],

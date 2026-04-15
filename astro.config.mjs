c4d0bfb1919cc84a0cd98fb5839db1f19ebf7511
Stanislas de Chevigné <standechevigne@gmail.com>
2026-04-15 07:49:03 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: 'https://be-ikigai.com',
+  integrations: [tailwind(), react(), sitemap({
+    i18n: {
+      defaultLocale: 'fr',
+      locales: {
+        fr: 'fr-FR',
+        en: 'en-US',
+      },
+    },
+    filter: (page) => !page.includes('/api/'),
+  })],
   i18n: {
     defaultLocale: "fr",
     locales: ["fr", "en"],
